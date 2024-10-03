c6d9d73687d06c6d1aadf0a76c8bbf9d1ad2df4e
vicentegalaz <vicentegalaz12@gmail.com>
2024-10-02 22:11:38 -0300

    comit 2

--- app/src/main/java/com/example/proshield/mapa.java
+++ app/src/main/java/com/example/proshield/mapa.java
@@ -1,12 +1,11 @@
 package com.example.proshield;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -25,7 +24,6 @@ public class mapa extends Fragment {
     private String mParam2;
 
     public mapa() {
-        // Required empty public constructor
     }
 
     /**
@@ -58,7 +56,7 @@ public class mapa extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_mapa, container, false);
     }
 }
